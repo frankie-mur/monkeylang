b0897fdfa8f1d8844a119ee5e9e7e1a5efb935e2
Frankie Murillo <frankidatank@gmail.com>
2024-04-22 22:23:09 -0700

    feat: add initial lexer and token with test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/frankie-mur/monkeylang
+
+go 1.22.0
---
+++ lexer/lexer.go
@@ -0,0 +1,111 @@
+package lexer
+
+import "github.com/frankie-mur/monkeylang/token"
+
+type Lexer struct {
+	input        string
+	position     int  // current position in input (points to current char)
+	readPosition int  // current reading position in input (after current char)
+	ch           byte // current char under examination
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	//Call readChar() so our lexer is in working state
+	l.readChar()
+	return l
+}
+
+// readChar reads the next character from the input string and updates the Lexer's state accordingly.
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		//ASCII code for the NUL character
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	l.skipWhitespace()
+
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case 0:
+		tok.Literal = ""
+		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Literal = l.readNumber()
+			tok.Type = token.INT
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, l.ch)
+		}
+	}
+
+	//Move position pointers to the next character
+	l.readChar()
+	return tok
+
+}
+
+// readIdentifier returns the identifier unitl the next non-letter character is encountered.
+func (l *Lexer) readIdentifier() string {
+	initialPosition := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[initialPosition:l.position]
+}
+
+// readNumber returns the number literal until the next non-digit character is encountered.
+func (l *Lexer) readNumber() string {
+	initialPosition := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[initialPosition:l.position]
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
+}
