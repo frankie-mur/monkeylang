cd718f5cc31443d49c67c2e26b5ab5a0b0528c36
Frankie Murillo <frankidatank@gmail.com>
2024-06-26 20:44:27 -0700

    feat: add support for array index expression

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -57,6 +57,17 @@ func Eval(node ast.Node, env *object.Enviroment) object.Object {
 
 		return evalInfixExpression(node.Operator, left, right)
 
+	case *ast.IndexExpression:
+		left := Eval(node.Left, env)
+		if isError(left) {
+			return left
+		}
+		index := Eval(node.Index, env)
+		if isError(index) {
+			return index
+		}
+		return evalIndexExpression(left, index)
+
 	case *ast.BlockStatement:
 		return evalBlockStaement(node, env)
 
@@ -168,6 +179,30 @@ func evalInfixExpression(
 	default:
 		return newError("unknown operator: %s %s %s", right.Type(), operator, left.Type())
 	}
+}
+
+func evalIndexExpression(left, index object.Object) object.Object {
+	switch {
+	case left.Type() == object.ARRAY_OBJ && index.Type() == object.INTEGER_OBJ:
+		return evalArrayIndexExpression(left, index)
+	default:
+		return newError("index operator not supported: %s", left.Type())
+	}
+}
+
+// evalArrayIndexExpression evaluates an index expression on an array object.
+// It takes an array object and an index object, and returns the element at the specified index.
+// If the index is out of bounds, it returns NULL.
+func evalArrayIndexExpression(array, index object.Object) object.Object {
+	arrayObject := array.(*object.Array)
+	idx := index.(*object.Integer).Value
+	max := int64(len(arrayObject.Elements) - 1)
+
+	if idx < 0 || idx > max {
+		return NULL
+	}
+
+	return arrayObject.Elements[idx]
 }
 
 func evalIntegerInfixExpression(
