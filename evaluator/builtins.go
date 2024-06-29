e8fafc276dc6f16fba0dc467fde03252348a4a44
Frankie Murillo <frankidatank@gmail.com>
2024-06-29 10:21:23 -0700

    feat: add new builtin function rest for array

--- evaluator/builtins.go
+++ evaluator/builtins.go
@@ -55,4 +55,24 @@ var builtins = map[string]*object.Builtin{
 			return arr.Elements[len-1]
 		},
 	},
+	"rest": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			if len(args) != 1 {
+				return newError("wrong number of arguments. got=%d, want=1", len(args))
+			}
+			if args[0].Type() != object.ARRAY_OBJ {
+				return newError("argument to `rest` must be ARRAY, got %s", args[0].Type())
+			}
+
+			arr := args[0].(*object.Array)
+			if len(arr.Elements) == 0 {
+				return NULL
+			}
+
+			newElements := make([]object.Object, len(arr.Elements)-1)
+			copy(newElements, arr.Elements[1:])
+
+			return &object.Array{Elements: newElements}
+		},
+	},
 }
